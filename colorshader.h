98279eb0101d18a4a762abe4cb2f6bcfc319876f
mofarrell <micpof@gmail.com>
2014-02-15 23:23:28 -0500

    Added colorshader and stuff that isnt finished.

---
+++ colorshader.h
@@ -0,0 +1,57 @@
+
+#ifndef COLORSHADER_H
+#define COLORSHADER_H
+
+#include "shader.h"
+
+static const GLchar *vertex_shader[] = {
+    "#version 330\n",
+    "layout(location = 0) in vec3 vertex;\n",
+    "layout(location = 1) in vec3 vertexColor;\n",
+    "out vec3 fragmentColor;\n",
+    "uniform mat4 model;\n",
+    "uniform mat4 vp;\n",
+    "void main(void) {\n",
+    "    gl_Position = vp * model * vec4(vertex.xyz, 1.f);\n",
+    "    fragmentColor = vertexColor;\n",
+    "}"
+};
+
+static const GLchar *fragment_shader[] = {
+    "#version 330\n",
+    "in vec3 fragmentColor;\n",
+    "out vec4 color;\n",
+    "void main() {\n",
+    "    color = vec4(fragmentColor, 1.0); // (pow(fragmentColor.r,2) + pow(fragmentColor.g,2) + pow(fragmentColor.b,2))/3.0);\n",
+    "}"
+};
+
+class ColorShader {
+ public:
+  GLuint modelLocation;
+  GLuint vpLocation;
+  glm::mat4 vp;
+  glm::mat4 model;
+  Shader_prog prog;
+
+  ColorShader()
+      : prog(vertex_shader, fragment_shader) {
+    prog();
+    modelLocation = glGetUniformLocation(prog, "model");
+    vpLocation = glGetUniformLocation(prog, "vp");
+  }
+  
+  void operator()() {
+    prog();
+    glUniformMatrix4fv(vpLocation, 1, GL_FALSE, glm::value_ptr(vp));
+    glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+  }
+  
+
+  ~ColorShader() {
+  }
+};
+
+
+#endif /* COLORSHADER_H */
+
---
+++ triangle.h
@@ -0,0 +1,136 @@
+
+#ifndef CUBE_H
+#define CUBE_H
+
+#include <OpenGL/gl3.h>
+#include <OpenGL/gl3ext.h>
+#include <OpenGL/glext.h>
+
+#include "colorshader.h"
+
+
+static const GLfloat vertices[] = {
+  -1.0f,-1.0f, 0.0f,
+  -1.0f,-1.0f, 1.0f,
+  -1.0f, 1.0f, 1.0f,
+};
+
+static const GLfloat colors[] = {
+  1.0f,  0.0f,  0.0f,
+  1.0f,  0.0f,  0.0f,
+  1.0f,  0.0f,  0.0f,
+  1.0f,  0.0f,  0.0f,
+  1.0f,  0.0f,  0.0f,
+  1.0f,  0.0f,  0.0f,
+
+  0.0f,  1.0f,  0.0f,
+  0.0f,  1.0f,  0.0f,
+  0.0f,  1.0f,  0.0f,
+  0.0f,  1.0f,  0.0f,
+  0.0f,  1.0f,  0.0f,
+  0.0f,  1.0f,  0.0f,
+
+  0.0f,  0.0f,  1.0f,
+  0.0f,  0.0f,  1.0f,
+  0.0f,  0.0f,  1.0f,
+  0.0f,  0.0f,  1.0f,
+  0.0f,  0.0f,  1.0f,
+  0.0f,  0.0f,  1.0f,
+
+  1.0f,  1.0f,  0.0f,
+  1.0f,  1.0f,  0.0f,
+  1.0f,  1.0f,  0.0f,
+  1.0f,  1.0f,  0.0f,
+  1.0f,  1.0f,  0.0f,
+  1.0f,  1.0f,  0.0f,
+
+  0.0f,  1.0f,  1.0f,
+  0.0f,  1.0f,  1.0f,
+  0.0f,  1.0f,  1.0f,
+  0.0f,  1.0f,  1.0f,
+  0.0f,  1.0f,  1.0f,
+  0.0f,  1.0f,  1.0f,
+
+  1.0f,  0.0f,  1.0f,
+  1.0f,  0.0f,  1.0f,
+  1.0f,  0.0f,  1.0f,
+  1.0f,  0.0f,  1.0f,
+  1.0f,  0.0f,  1.0f,
+  1.0f,  0.0f,  1.0f,
+};
+
+class Cube {
+ public:
+  GLuint vbo;
+  GLuint cbo;
+  GLuint vao;
+  ColorShader shader;
+  glm::mat4 model;
+
+  Cube () : shader() {
+    model = glm::mat4(1.0f);
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+
+    //////////////////////////////
+    // Vertice coords
+    //////////////////////////////
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vbo);
+
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(
+            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+            );
+    glEnableVertexAttribArray(0);
+    //////////////////////////////
+    // Color coords
+    //////////////////////////////
+    glGenBuffers(1, &cbo);
+
+    glBindBuffer(GL_ARRAY_BUFFER, cbo);
+
+    // Give our colors to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    glVertexAttribPointer(
+            1,                  // attribute 1. No particular reason for 1, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+            );
+    glEnableVertexAttribArray(1);
+
+
+    glBindVertexArray(0);
+  }
+
+  void draw(glm::mat4 viewProjection) {
+    shader.vp = viewProjection;
+    shader.model = model;
+    shader();
+
+    glBindVertexArray(vao);
+
+    glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices)/sizeof(GLfloat)/3); // Starting from vertex 0; vertices total -> 1 triangle
+
+    glBindVertexArray(0);
+  }
+
+  ~Cube () {
+  }
+};
+
+#endif /* CUBE_H */
+
